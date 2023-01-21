79f8c39603a07d40b0222c60f614d4744ca03f6b
Skvortsov Mikhail <skvorcov97.97@mail.ru>
2023-01-21 20:12:54 +0500

    Перешел с int на string в Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -4,38 +4,38 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-Console.WriteLine ("Введите число:");
-int day = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine("Введите число:");
+string day = (Console.ReadLine());
 
-if (day == 1)
-{ 
+if (day == "1")
+{
     Console.WriteLine("Понедельник");
 }
-else if (day == 2)
-{ 
+else if (day == "2")
+{
     Console.WriteLine("Вторник");
 }
-else if (day ==3)
-{ 
+else if (day == "3")
+{
     Console.WriteLine("Среда");
 }
-else if (day == 4)
-{ 
+else if (day == "4")
+{
     Console.WriteLine("Четверг");
 }
-else if (day == 5)
-{ 
+else if (day == "5")
+{
     Console.WriteLine("Пятница");
 }
-else if (day == 6)
-{ 
+else if (day == "6")
+{
     Console.WriteLine("Суббота");
 }
-else if (day == 7)
-{ 
+else if (day == "7")
+{
     Console.WriteLine("Воскресенье");
 }
-else 
+else
 {
-    Console.WriteLine("Error!");
+    Console.WriteLine("Error!Введите число от 1 до 7.");
 }
